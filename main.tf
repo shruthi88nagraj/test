7b0f890d21c105168ea168299489c37b869a5d36
shruthi88nagraj <86025410+shruthi88nagraj@users.noreply.github.com>
2021-06-22 17:18:28 -0700

    Updated main.tf
    updated with load balancer, security_backend_pool

--- main.tf
+++ main.tf
@@ -5,6 +5,9 @@ provider "azurerm" {
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
+  tags = {
+    environment = "${var.rg_tag}"
+  }
 }
 
 resource "azurerm_virtual_network" "main" {
@@ -32,8 +35,21 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+resource "azurerm_managed_disk" "main" {
+  name                    = "${var.prefix}-md"
+  location                = "${var.location}"
+  resource_group_name     = azurerm_resource_group.main.name
+  storage_account_type    = "Standard_LRS"
+  create_option           = "Empty"
+  disk_size_gb            = "1"
+
+  tags = {
+    environment = "UdacityProject1"
+  }
+}
+
 # Security group to deny inbound traffic from Internet
-resource "azurerm_network_security_group" "tf-guide-sg" {
+resource "azurerm_network_security_group" "main" {
   name                = "${var.prefix}-sg"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
@@ -50,8 +66,47 @@ resource "azurerm_network_security_group" "tf-guide-sg" {
     destination_address_prefix = "VirtualNetwork"
   }
 }  
+resource "azurerm_network_interface_security_group_association" "main" {
+  count                          = "${var.countVm}"
+  network_interface_id =    azurerm_network_interface.main.id
+  network_security_group_id      = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_public_ip" "main" {
+  name           = "${var.prefix}-PublicIp"
+  resource_group_name     = azurerm_resource_group.main.name
+  location                = "${var.location}"
+  allocation_method       = "Static"
+  tags = {
+    environment = "UdacityProject1"
+  }
+}
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "primary"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name             = azurerm_resource_group.main.name
+  loadbalancer_id                 = azurerm_lb.main.id
+  name                            = "${var.prefix}-lbBackendpool"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "main"{
+  network_interface_id =    azurerm_network_interface.main.id
+  ip_configuration_name    ="UdProConfiguration"
+  backend_address_pool_id  = azurerm_lb_backend_address_pool.main.id
+}
+
 resource "azurerm_linux_virtual_machine" "main" {
-  count                           = "${var.count}"
+  count                           = "${var.countVm}"
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
@@ -73,6 +128,9 @@ resource "azurerm_linux_virtual_machine" "main" {
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
+  }
+  tags = {
+    environment = "UdacityProject1"
   }
 }
 
